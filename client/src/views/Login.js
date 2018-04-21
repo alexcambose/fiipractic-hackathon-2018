57d13832bd34b26d99fadc0abc3825fdb19c7aad
Alexandru Cambose <alexcambose@yahoo.com>
2018-04-21 11:01:30 +0300

    Login Registrer

---
+++ client/src/views/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+
+class Login extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div>Login</div>
+        );
+    }
+}
+
+Login.propTypes = {};
+
+export default Login;
---
+++ client/src/views/Register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+
+class Register extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div>Register</div>
+        );
+    }
+}
+
+Register.propTypes = {};
+
+export default Register;
